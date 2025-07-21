fa9de3a38cbfefdd4d291e1eaefd54ea8f815288
Joshuadalmas <joshuadalmas848@gmail.com>
2025-07-21 18:44:53 +0300

    Error fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from "src/App";
+import App from "@/App";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
